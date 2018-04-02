327bb3875eddc74eff4f1643f3bf80526b0e7239
Liber17321 <tianwh3721@gmail.com>
2018-04-02 11:58:31 +0800

    config sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'rooms#show'
 
-
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # mount ActionCable.server => '/cable'
 end
